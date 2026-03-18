c4d2fbb4882ccb094b92f6997430fb90db4c14d8
jathin-08 <allamjathin05@gmail.com>
2026-03-18 09:13:45 +0530

    Update sum_of_digits tests

--- IV-SEM/Python/Assignments/M02_Logic_Building_And_Patterns/AST03/test_task.py
+++ IV-SEM/Python/Assignments/M02_Logic_Building_And_Patterns/AST03/test_task.py
@@ -1,16 +1,16 @@
 import unittest
-from task import reverse_number
+from task import sum_of_digits
 
 class TestAssignment(unittest.TestCase):
 
     def test_basic(self):
-        self.assertEqual(reverse_number(123), 321)
+        self.assertEqual(sum_of_digits(123), 6)
 
-    def test_two_digits(self):
-        self.assertEqual(reverse_number(45), 54)
+    def test_single(self):
+        self.assertEqual(sum_of_digits(5), 5)
 
-    def test_zero_end(self):
-        self.assertEqual(reverse_number(120), 21)
+    def test_with_zero(self):
+        self.assertEqual(sum_of_digits(105), 6)
 
 if __name__ == "__main__":
     unittest.main()
